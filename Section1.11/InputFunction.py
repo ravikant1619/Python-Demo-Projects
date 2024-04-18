bc791c4a6ac0ab09c718a891ddc3b0d21e1e1871
ravikant1619 <guptarajravi19092000@gmail.com>
2024-04-19 02:03:56 +0530

    inputFunction

---
+++ Section1.11/InputFunction.py
@@ -0,0 +1,9 @@
+#input()function
+#input("A prompt for the user")
+#when we print this code at last the mouse cursor will show
+
+
+#input() will get user input in console
+# Then print() will print the world "Hello" and the user input 
+
+print("Hello " +  input("What is your name"))
