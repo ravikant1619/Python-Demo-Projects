951a64d17c86ca6461d03d7b5ca0ce80f27ba5f3
ravikant1619 <guptarajravi19092000@gmail.com>
2024-04-19 00:25:01 +0530

    string

---
+++ SECTION1.9/String.py
@@ -0,0 +1,15 @@
+"using of \n"
+
+"for example write hello 4 times --- we write like this"
+
+print("Hello")
+print("Hello")
+print("Hello")
+print("Hello")
+
+"for saving the time we use the \n"
+
+print("Hello!\nHello!\nHello!\nHello!")
+
+" strings are immutable, meaning that once created, they cannot be changed"
+"HELLO + WORLD BY STRING IT WILL HelloWorld"
--- hello world/section1.8/printModifier.py
+++ hello world/section1.8/printModifier.py
@@ -1,9 +1,3 @@
-print("1. Min 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
-print("2. Knead the dough for 10 minutes.") 
-print("3. Add 3g of Salt.")
-print("4. Leave to rise for 2 hours.")
-print("5. Bake at 200 degrees C for minutes.")
-
 "Print Modifier"
 
 print("A 'single quote' inside a double quote")
