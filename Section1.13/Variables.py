25c82af058faf16b2d14725160465aff39a96b90
ravikant1619 <guptarajravi19092000@gmail.com>
2024-04-19 22:34:51 +0530

    variables

---
+++ Section1.13/Variables.py
@@ -0,0 +1,25 @@
+name = "Jack"
+print(name)
+
+name = "Ravi"
+print(name)
+
+
+name = input("What is your name?")
+length = len(name)
+print(length)
+
+#lenght command is used to count the number of the letter
+#len is short word of the length
+
+#Example
+#There are two variable a and b from the input
+a = input()
+b = input()
+#Write your code below this line 
+c=a 
+a=b
+b=c
+
+print("a: " + a)
+print("b: " + b)
