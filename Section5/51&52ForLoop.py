cf19fc17bd459404fb28388669ee166827c2ebca
ravikant1619 <guptarajravi19092000@gmail.com>
2024-04-28 23:40:33 +0530

    Loop with python lists

---
+++ Section5/51&52ForLoop.py
@@ -0,0 +1,23 @@
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + "Pie")
+    print(fruits)
+
+#52CodingExercise[Average Height]
+student_heights = input().split()
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+#code
+total_height = 0
+for height in student_heights:
+    total_height += height
+print(f"total height = {total_height}")
+
+number_of_students = 0
+for student in student_heights:
+    number_of_students += 1
+print(f"number of students = {number_of_students}")
+
+average_height = round(total_height / number_of_students)
+print(f"average height = {average_height}")
