65f23f2d31fd819dd1972a1ac0ad5d34b37c6f6d
ravikant1619 <guptarajravi19092000@gmail.com>
2024-05-04 23:35:37 +0530

    Indentation In Python

---
+++ Section6/60DefinigFunctions.py
@@ -0,0 +1,26 @@
+#In this we learn that how to make the in short line so that it will good to read it 
+#for example 
+#we have to move left for 4 times 
+#we write this command  def turn_left()
+#turn_left()
+#turn_left()
+#turn_left()
+#turn_left()
+
+
+#question
+
+def turn_right():
+    turn_left() # type: ignore
+    turn_left() # type: ignore
+    turn_left() # type: ignore
+
+#draw square
+turn_left() # type: ignore
+move()     # type: ignore
+turn_right()
+move() # type: ignore
+turn_right()
+move() # type: ignore
+turn_right()
+move() # type: ignore
---
+++ Section6/62Indentation.py
@@ -0,0 +1,12 @@
+#Indentation means that we will see the space in every code in start 
+#for example 
+
+def my_function():
+    if sky == "clear": # type: ignore
+        #4 space
+        print("blue")
+        #8 space
+    elif sky == "cloudy": # type: ignore
+        print("grey")
+    print("Hello")
+print("World")            
