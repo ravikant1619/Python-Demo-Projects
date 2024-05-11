c61fad7905eba4b82a7a4c1b6b0f4d6f263d7226
ravikant1619 <guptarajravi19092000@gmail.com>
2024-05-11 23:33:40 +0530

    Last Code

--- Section8/89LastCode.py
+++ Section8/89LastCode.py
@@ -14,7 +14,7 @@ def caesar(start_text, shift_amount, cipher_direction):
   print(f"Here's the {cipher_direction}d result: {end_text}")
 
 #TODO-1: Import and print the logo from art.py when the program starts.
-from art import logo
+from art import logo # type: ignore
 print(logo)
 
 #TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
