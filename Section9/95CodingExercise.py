2e8cd53ede710e39da6b91a0ab167f1c9c835391
ravikant1619 <guptarajravi19092000@gmail.com>
2024-05-12 20:27:19 +0530

    Exercise Coding

---
+++ Section9/95CodingExercise.py
@@ -0,0 +1,26 @@
+country = input() # Add country name
+visits = int(input()) # Number of visits
+list_of_cities = eval(input()) # create list from formatted string
+travel_log = [
+  {
+    "country": "France",
+    "visits": 12,
+    "cities": ["Paris", "Lille", "Dijon"]
+  },
+  {
+    "country": "Germany",
+    "visits": 5,
+    "cities": ["Berlin", "Hamburg", "Stuttgart"]
+  },
+]
+# Your code here 👇
+def add_new_country(name, times_visited, cities_visited):
+  new_country = {}
+  new_country["country"] = name
+  new_country["visits"] = times_visited
+  new_country["cities"] = cities_visited
+  travel_log.append(new_country)
+
+add_new_country(country, visits, list_of_cities)
+print(f"I've been to {travel_log[2]['country']} {travel_log[2]['visits']} times.")
+print(f"My favourite city was {travel_log[2]['cities'][0]}.")
