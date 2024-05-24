93927041e1cfbb135c85f5740e6a9f85a380c792
ravikant1619 <guptarajravi19092000@gmail.com>
2024-05-24 23:31:03 +0530

    ODD AND EVEN EXERCISE

---
+++ Section13/131CodingExercise.py
@@ -0,0 +1,11 @@
+# Which number do you want to check?
+number = int(input())
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+  
+# Error line 4: if number % 2 = 0:
+# Remember that single "=" is assignment.
+# Double "==" is checking for equality.
