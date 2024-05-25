4bfbcdc790ef4ee62773c29f12750be9a77b084f
ravikant1619 <guptarajravi19092000@gmail.com>
2024-05-26 00:11:12 +0530

    Complete Code

--- Section16/152CompleteProject.py
+++ Section16/152CompleteProject.py
@@ -1,6 +1,6 @@
-from menu import Menu
-from coffee_maker import CoffeeMaker
-from money_machine import MoneyMachine
+from menu import Menu # type: ignore
+from coffee_maker import CoffeeMaker # type: ignore
+from money_machine import MoneyMachine # type: ignore
 
 money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
